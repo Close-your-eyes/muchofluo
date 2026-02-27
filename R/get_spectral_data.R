731b3eb980733935feb522d0588bf7e400696289
Close-your-eyes <vonskopnik@pm.me>
2026-02-27 11:08:21 +0100

    up

--- R/get_spectral_data.R
+++ R/get_spectral_data.R
@@ -1,19 +1,17 @@
-#' Title
+#' Get spectral data from internal package data
 #'
-#' @param fluos
-#' @param spectra_df
-#' @param maxima_df
-#' @param progress
+#' @param fluos fluorochrome names, will be matched by query_fluo
+#' @param spectra_df data frame with spectral data
+#' @param maxima_df data frame with data on max emission and excitation
 #'
-#' @returns
+#' @returns list of df
 #' @export
 #'
 #' @examples
 #' out <- get_spectral_data(c("A647", "FITC"))
 get_spectral_data <- function(fluos,
                               spectra_df = vroom::vroom(system.file("extdata", "spectra.tsv.gz", package = "muchofluo"), show_col_types = F, progress = F),
-                              maxima_df = vroom::vroom(system.file("extdata", "em_ex_maxima.tsv", package = "muchofluo"), show_col_types = F, progress = F),
-                              progress = F) {
+                              maxima_df = vroom::vroom(system.file("extdata", "em_ex_maxima.tsv", package = "muchofluo"), show_col_types = F, progress = F)) {
 
   # no error catching
 
@@ -28,16 +26,23 @@ get_spectral_data <- function(fluos,
     sources <- intersect(unique(data$source),
                          unique(maxdata$source))
     purrr::map(purrr::set_names(sources), function(y) {
-      data <-
+      data2 <-
         data |>
         dplyr::filter(source == y) |>
         tidyr::pivot_longer(cols = c(em, ex), names_to = "type", values_to = "value") |>
         tidyr::drop_na()
+      attr(data2, "max") <- dplyr::filter(maxdata, source == y)
+      return(data2)
     })
-  }, .progress = progress)
+  })
 
   spec_data <- brathering::list_invert(spec_data)
-  spec_data <- purrr::map(spec_data, ~dplyr::bind_rows(.x))
+  spec_data2 <- purrr::map(spec_data, ~dplyr::bind_rows(.x))
+  maxes <- purrr::map(spec_data, ~purrr::map_dfr(.x, ~attr(.x, "max")))
+  spec_data2 <- purrr::map2(spec_data2, maxes, function(x,y) {
+    attr(x, "max") <- y
+    return(x)
+  })
 
-  return(spec_data)
+  return(spec_data2)
 }
